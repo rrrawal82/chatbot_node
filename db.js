552a1d2b3648d55c69a52ad91fe95a99818e5feb
rrrawal82 <38749745+rrrawal82@users.noreply.github.com>
2024-08-23 12:54:09 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,5 +5,12 @@ export const db=mysql.createConnection({
     user:"root",
     password:"",
     //database:"blog"
-    database:"audiochatbot"
+    database:"audiochatbot",
+    timezone: '+0800',
+    connectionLimit: 10,
+    connectTimeout: 10000,
+    waitForConnections: true,
+    queueLimit: 0
+
+    
 })
