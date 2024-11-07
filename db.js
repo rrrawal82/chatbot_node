c30962bfe4117cd4aebde8f6defe010131b37eef
rrrawal82 <38749745+rrrawal82@users.noreply.github.com>
2024-11-07 13:59:28 +0530

    vvv

--- db.js
+++ db.js
@@ -2,9 +2,8 @@ import mysql from "mysql"
 
 export const db=mysql.createConnection({
     host:"localhost",
-    user:"root",
-    password:"",
-    //database:"blog"
+    user:"my_app_user",
+    password:"MyNewPass1!",
     database:"audiochatbot",
     timezone: '+0800',
     connectionLimit: 10,
